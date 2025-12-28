91bbca47df7800597757df3e9d29410b314d518d
Metwet <tselm.matv@gmail.com>
2025-12-28 21:27:20 +0300

    fix: prisma studio

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 /**
  * AppModule - главный модуль приложения
- * 
+ *
  * Здесь мы импортируем все остальные модули нашего приложения
  * imports: [UsersModule] - подключаем модуль пользователей
  */
 @Module({
-  imports: [UsersModule], // Импортируем модуль пользователей
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
