2ba869a8d81046505b91c8c7328998c5c95348ff
Metwet <tselm.matv@gmail.com>
2026-02-23 00:59:09 +0300

    test pre-commit (N2)

--- frontend/src/app/_layouts/main-layout.tsx
+++ frontend/src/app/_layouts/main-layout.tsx
@@ -16,7 +16,7 @@ interface IMainLayoutProps {
 const MainLayout: FC<IMainLayoutProps> = ({ children }) => {
   const [theme, setTheme] = useState<TTheme>("light");
   const [isMounted, setIsMounted] = useState(false);
-  console.log("test pre-coomit") {};
+  console.log("test pre-coomit") {{}};
   
 
   const currentTheme = useMemo(
